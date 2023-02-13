9f2478a4378d742c46f347c1fd83442a6cc4e670
Christal Chen <christalchen1209@gmail.com>
2023-02-13 15:58:08 +0800

    feat: app crashed

--- app.js
+++ app.js
@@ -1,47 +1,52 @@
-const express = require('express')
-const session = require('express-session')
-const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
-const flash = require('connect-flash')
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-const routes = require('./routes')
-
-const usePassport = require('./config/passport')
-require('./config/mongoose')
-
+const express = require("express")
+const exphbs = require("express-handlebars")
+const methodOverride = require("method-override")
+const session = require("express-session")
+const flash = require("connect-flash")
+const usePassport = require("./config/passport")
+const routes = require("./routes/index")
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.engine('hbs', exphbs({ extname: '.hbs' }))
-app.set('view engine', 'hbs')
 
-app.use(session({
-  secret: 'ThisIsMySecret',
-  resave: false,
-  saveUninitialized: true
-}))
+require("./config/mongoose")
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+const hbs = exphbs.create({
+  extname: "hbs",
+  defaultLayout: "main",
+})
+app.engine("hbs", hbs.engine)
+app.set("view engine", "hbs")
 
-usePassport(app)
 
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: `ThisIsMySecret`,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 app.use(flash())
+
+usePassport(app);
+
+
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
-  res.locals.success_msg = req.flash('success_msg')
-  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.success_msg = req.flash("success_msg")
+  res.locals.warning_msg = req.flash("warning_msg")
+  res.locals.error = req.flash("error")
   next()
 })
 
+
 app.use(routes)
 
+
 app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${PORT}`)
+  console.log(`App is listening to http://localhost:${PORT}`)
 })
